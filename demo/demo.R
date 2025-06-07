da40c97e8307affba54383e236c28f46b8602f0d
Isaac Liu <ijyliu@gmail.com>
2025-06-07 19:50:09 -0400

    note on compliation options

--- demo/demo.R
+++ demo/demo.R
@@ -51,7 +51,9 @@ stattotex("SaleDir", word, "demoNums.tex")
 
 # In LaTeX
 
-# We can now compile a document making use of these numbers. Check out the `Demo Report.tex` file in this folder, and the associated `Demo Report.pdf` created.
+# We can now compile a document making use of these numbers. 
+# I am doing this in R for convenience, but you can also do this directly via other means.
+# Check out the `Demo Report.tex` file in this folder, and the associated `Demo Report.pdf` created.
 install.packages('tinytex', repos = "https://cloud.r-project.org")
 if (!tinytex::is_tinytex()) {
   tinytex::install_tinytex() 
